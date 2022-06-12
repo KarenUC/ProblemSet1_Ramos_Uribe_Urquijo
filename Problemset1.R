5b971745974374d13abcc3b7545839c43a5b3e39
Paula Ramos <p.ramosj@uniandes.edu.co>
2022-06-12 12:10:52 -0500

    Problem set 1

---
+++ Problemset1.R
@@ -0,0 +1,18 @@
+# Paula Ramos, Karen Uribe y Juan Urquijo
+# update: 12-06-2022
+## install pacman
+##install.packages("pacman")
+rm(list=ls())
+## llamar librerias de la sesion
+require(pacman)
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       caret)  # Classification And REgression Training
+
+
+## data adquisition
+library(tidyverse)
+library(rvest)
+Data1 <- read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/page1.html")
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProblemSet1_Ramos_Uribe_Urquijo
+
+En este repositorio se encuentra el desarrollo del primer problem set de la asignatura BigData-MECA, desarrollado por Ramos Paula, Uribe Karen y Urquijo Juan
  
